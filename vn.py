4b9b318b3efaa98c082cdd3a5552535d3d803970
Titus Lim <tituslhy@gmail.com>
2025-06-11 13:42:19 +0800

    Update vn.py

--- vn.py
+++ vn.py
@@ -6,7 +6,7 @@ import yaml
 
 from vanna.openai.openai_chat import OpenAI_Chat
 from vanna.chromadb.chromadb_vector import ChromaDB_VectorStore
-from openai import AzureOpenAI
+from openai import OpenAI
 from tqdm import tqdm
 
 _ = load_dotenv(find_dotenv())
@@ -38,12 +38,7 @@ class MyVanna(ChromaDB_VectorStore, OpenAI_Chat):
         ChromaDB_VectorStore.__init__(self, config=config)
         OpenAI_Chat.__init__(
             self,
-            client = AzureOpenAI(
-                azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
-                azure_deployment=os.environ["AZURE_OPENAI_GPT4O_DEPLOYMENT_NAME"],
-                api_version=os.environ["AZURE_API_VERSION"],
-                api_key=os.environ["AZURE_OPENAI_API_KEY"],                
-            ),
+            client = OpenAI(),
             config = config
         )
 
